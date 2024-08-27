3599c3ad3ce77ea6a1a86aa6983366c9cd7b4490
Alexander Sarango <alexandersarango@gmail.com>
2024-08-27 10:05:15 -0500

    new designed

---
+++ script.js
@@ -0,0 +1,30 @@
+const navEl = document.querySelector('.nav');
+
+window.addEventListener('scroll',() => {
+    if (window.scrollY > 50) {
+        navEl.classList.add('nav-scrolled');
+    }
+    else if (window.scrollY <= 50) {
+        navEl.classList.remove('nav-scrolled');
+    }
+});
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
